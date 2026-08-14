3c1d5e8fc5f2c645dd2d6ea7639fa453f38b60d5
g.valencia <g.valencia@ebc.edu.mx>
2026-08-13 21:20:41 -0600

    Segundo commit - Models, Views, DAO

---
+++ pedidos/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from pedidos.models import Producto, Pedido
+
+class ProductoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Producto
+        fields = '__all__'
+
+class PedidoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pedido
+        fields = '__all__'
